171213a505acd0faad7a5aa137c828579af778bf
royhok@gmail.com <royhok@gmail.com>
2019-01-14 15:54:38 +0900

    2.3.8 Button을 XAML로 정의하기

---
+++ CustomXamlButton/CustomXamlButton/UseCustomClass.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows;
+
+namespace CustomXamlButton
+{
+
+public partial class UseCustomClass : Window
+    {
+        [STAThread]
+        public static void Main()
+        {
+            Application app = new Application();
+            app.Run(new UseCustomClass());
+        }
+        public UseCustomClass()
+        {
+            InitializeComponent();
+
+            //버튼 2개를 추가로 생성
+            for (int i = 0; i < 2; i++)
+            {
+                CenteredButton b = new CenteredButton();
+                b.Content = "Button " + (i + 1);
+                mystack.Children.Add(b);
+            }
+        }
+    }
+}
