137387af945427dcc3912c295336064172e61bd4
royhok@gmail.com <royhok@gmail.com>
2019-01-14 11:17:31 +0900

    WPF HelloWorld~ Application 관련

---
+++ HelloWorld1/HelloWorld1/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HelloWorld1
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //이벤트 핸들러, 이벤트 가입자
+        private void btnSayHello_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("안녕~" + txtName.Text);
+            MessageBox.Show(string.Format("안녕~{0}", txtName.Text));
+            MessageBox.Show($"안녕~{txtName.Text}");
+        }
+    }
+}
---
+++ HelloWorld1/HelloWorld1/MyMain.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace HelloWorld1
+{
+    class MyMain
+    {
+        [STAThread]
+        public static void Main()
+        {
+            Application app = new Application();
+            app.Run(new MainWindow());
+        }
+        
+    }
+}
