79eed7e2779c1e6fc433c3e8bbfeee7a90164c1f
royhok@gmail.com <royhok@gmail.com>
2019-01-15 16:09:56 +0900

    6.9 InotifyPropertyChanged 인터페이스, PropertyChanged 이벤트를 통한 데이터 바인 딩

---
+++ DigitalClock/DigitalClock/ClockTicker2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+using System.ComponentModel;
+
+namespace DigitalClock
+{
+    class ClockTicker2 : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string DateTime
+        {
+            get { return System.DateTime.Now.ToString("yyyyMMddHHmmss"); }
+        }
+
+        public ClockTicker2()
+        {
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Tick += TimerOnTick;
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Start();
+        }
+
+        private void TimerOnTick(object sender, EventArgs e)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs("DateTime"));
+            }
+        }
+    }
+}
