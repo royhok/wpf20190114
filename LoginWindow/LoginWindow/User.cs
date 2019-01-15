a5ab78f0a66aefd6531a6d5c1d96cc2c4b78dd59
royhok@gmail.com <royhok@gmail.com>
2019-01-15 16:27:54 +0900

    6.9 _ INotifyPropertyChanged 인터페이스를 이용한 데이터 바인딩 예제2

---
+++ LoginWindow/LoginWindow/User.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace LoginWindow
+{
+    public class User : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _firstname;
+
+        public string FirstName
+        {
+            get { return _firstname; }
+            set
+            {
+                _firstname = value;
+                RaisePropertyChange();
+            }
+        }
+
+        private string _lastname;
+
+        public string LastName
+        {
+            get { return _lastname; }
+            set
+            {
+                _lastname = value;
+                RaisePropertyChange();
+            }
+        }
+
+        public void RaisePropertyChange([CallerMemberName] string propertyname = null)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyname));
+            }
+        }
+    }
+}
