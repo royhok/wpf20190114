80645fb155420264bfa4d2014f4cc20d14a3baa9
royhok@gmail.com <royhok@gmail.com>
2019-01-15 15:10:40 +0900

    6.6 사용자정의 엘리먼트와 ScrollBar 데이터 바인딩

---
+++ CustomElementBinding/CustomElementBinding/MyElement.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Media;
+
+namespace CustomElementBinding
+{
+    class MyElement : FrameworkElement
+    {
+        // 의존 프로퍼티 선언
+        // 데이터 바인딩 통보 메커니즘은 이를 통해 저절로 확보된다.
+        public static DependencyProperty NumberProperty;
+        // static 생성자를 통한 의존 프로퍼티 생성
+        static MyElement()
+        {
+            NumberProperty =
+            DependencyProperty.Register("Number", typeof(double),
+            typeof(MyElement),
+            new FrameworkPropertyMetadata(0.0,
+            FrameworkPropertyMetadataOptions.AffectsRender));
+        }
+
+        // DependencyProperty를 CLR 프로퍼티로 노출
+        public double Number
+        {
+            set { SetValue(NumberProperty, value); }
+            get { return (double)GetValue(NumberProperty); }
+        }
+        // MeasureOverride를 오버라이딩해 Size를 리턴하는데 원래 이함수는 자식
+        // 엘리먼트를 돌면서 크기를 계산하고 업데이트하는 메소드 이다.
+        // 이함수를 재정의 안하면 자식 컨트롤의 크기가 이상해 질 수 있다.
+        // MyElement의 크기를 측정해서 FrameworkElement 파생클래스(MyElement)
+        // 의 Size를 리턴하는 것이다.레이아웃을 만들 때 자동으로 호출된다.
+        protected override Size MeasureOverride(Size sizeAvailable)
+        {
+            return new Size(200, 50);
+        }
+        // Number 프로퍼티를 보여주는 OnRender
+        protected override void OnRender(DrawingContext dc)
+        {
+            dc.DrawText(
+            new FormattedText(Number.ToString(),
+            CultureInfo.CurrentCulture, FlowDirection.LeftToRight,
+                new Typeface("Times New Roman"), 12,
+                 SystemColors.WindowTextBrush),
+                 new Point(0, 0));
+        }
+    }
+}
