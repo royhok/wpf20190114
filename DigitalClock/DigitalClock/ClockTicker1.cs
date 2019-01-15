69d3a5008891f8312c3cf1752dfb44a6cb9681af
royhok@gmail.com <royhok@gmail.com>
2019-01-15 15:41:45 +0900

    6.8 Source, StaticResource 를 통한 데이터 바인딩

---
+++ DigitalClock/DigitalClock/ClockTicker1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace DigitalClock
+{
+    class ClockTicker1 : DependencyObject
+    {
+        public static DependencyProperty DateTimeProperty =
+            DependencyProperty.Register("DateTime", typeof(string), typeof(ClockTicker1));
+
+        public string DateTime
+        {
+            get { return (string)GetValue(DateTimeProperty); }
+            set { SetValue(DateTimeProperty, value); }
+        }
+
+        public ClockTicker1()
+        {
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Tick += TimerOnTick;
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Start();
+        }
+
+        private void TimerOnTick(object sender, EventArgs e)
+        {
+            DateTime = System.DateTime.Now.ToString("yyyyMMdd HHmmss");
+        }
+    }
+}
