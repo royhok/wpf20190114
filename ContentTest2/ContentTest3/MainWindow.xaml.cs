70a3d27b9cc854cb8976bc9865c056178cc1f889
royhok@gmail.com <royhok@gmail.com>
2019-01-15 11:08:21 +0900

    5.4 컨텐트(Content), FontFamily, FontSize _ ContentTest3

---
+++ ContentTest2/ContentTest3/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ContentTest3
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Title = "Show My Face";
+
+            Uri uri = new Uri("http://www.charlespetzold.com/PetzoldTattoo.jpg");
+
+            //이미지를 메모리에 로딩
+            BitmapImage bitmap = new BitmapImage(uri);
+            Image img = new Image();
+            img.Source = bitmap;
+            Content = img;
+        }
+    }
+}
