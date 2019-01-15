6f827a5fc8efa447578a918f4c2e3282d332ed59
royhok@gmail.com <royhok@gmail.com>
2019-01-15 10:26:47 +0900

    5.3 브러시(Brush), Gradient Brush(LinearGradientBrush, RadialGradientBrush)

---
+++ GradientBrushTest/GradientBrushTest/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace GradientBrushTest
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        RadialGradientBrush brush;
+        double angle;
+        DispatcherTimer tmr;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //Title = "Gradiate the Brush";
+
+            //LinearGradientBrush brush = new LinearGradientBrush(Colors.Red, Colors.Blue, new Point(0, 0), new Point(1, 1));
+            //Background = brush;
+
+            Title = "Rotate the Gradient Origin";
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            Width = 384;
+            Height = 384;
+
+            brush = new RadialGradientBrush(Colors.White, Colors.Blue);
+            brush.Center = brush.GradientOrigin = new Point(0.5, 0.5);
+            brush.RadiusX = brush.RadiusY = 0.1;
+            brush.SpreadMethod = GradientSpreadMethod.Repeat;
+            Background = brush;
+
+            // 타이머
+            tmr = new DispatcherTimer();
+            tmr.Interval = TimeSpan.FromMilliseconds(100);
+            tmr.Tick += TimerOnTick;
+            tmr.Start();
+        }
+
+        private void TimerOnTick(object sender, EventArgs e)
+        {
+            Point pt = new Point(0.5 + 0.05 * Math.Cos(angle), 0.5 + 0.05 * Math.Sin(angle));
+            brush.GradientOrigin = pt;
+            angle = Math.PI / 6;
+        }
+
+        protected override void OnMouseDown(MouseButtonEventArgs e)
+        {
+            tmr.Stop();
+        }
+    }
+}
