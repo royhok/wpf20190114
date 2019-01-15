15d2f57ba0a527235957b298f3877f4e2ab51fea
royhok@gmail.com <royhok@gmail.com>
2019-01-15 14:43:28 +0900

    6.5 DataContext 를 이용한 TwoWay 데이터 바인딩 실습

---
+++ DataContextTwoWayBinding/DataContextTwoWayBinding/Emp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataContextTwoWayBinding
+{
+    public class Emp
+    {
+        public string Ename
+        {
+            get; set;
+        }
+
+        public string City
+        {
+            get; set;
+        }
+    }
+}
