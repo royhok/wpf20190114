b62cb045e618024e5e18159a4b003d3095ef9bd7
royhok@gmail.com <royhok@gmail.com>
2019-01-16 16:11:44 +0900

    9.1. WPF Style

---
+++ StyleTest/StyleTest/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StyleTest
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = e.Source as Button;
+            //MessageBox.Show(btn.Content + " 클릭되었습니다.", Title);
+            MessageBox.Show($"{btn.Content}({btn.Name}) 클릭되었습니다.", Title);
+        }
+    }
+}
